3139c83d7b02d3606c0883f70c862785bf453e89
RiyanAditiya <riyanaditiyasaputra@gmail.com>
2025-11-12 14:21:56 +0700

    Mengubah cust-booking-index.blade.php

--- resources/views/livewire/customer/cust-booking-index.blade.php
+++ resources/views/livewire/customer/cust-booking-index.blade.php
@@ -28,7 +28,7 @@
                     x-transition:enter-end="opacity-100 translate-y-0"
                     x-transition:leave="transition ease-in duration-300"
                     x-transition:leave-start="opacity-100 translate-y-0"
-                    x-transition:leave-end="opacity-0 translate-y-[-10px]" x-init="setTimeout(() => show = false, 5000)" class="relative mb-4">
+                    x-transition:leave-end="opacity-0 translate-y-[-10px]" x-init="setTimeout(() => show = false, 3000)" class="relative mb-4">
 
                     <div class="flex items-center p-4 text-sm font-semibold border rounded-lg shadow-md
                         text-{{ $color }}-800 border-{{ $color }}-400 bg-{{ $color }}-100 
